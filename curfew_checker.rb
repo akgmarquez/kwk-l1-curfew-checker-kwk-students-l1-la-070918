d89b5a435765862510180c400624eaf7a6747947
akgmarquez <akgmarquez@gmail.com>
2018-07-11 16:30:59 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,8 +1,8 @@
-def curfew_checker(time<=curfew)
+def curfew_checker(time)
 if 12 <= 11
   puts "You're in trouble! Better get home quick!"
 else
   puts "You can keep having fun!"
 end
 
-puts curfew_checker(time<=curfew)
+puts curfew_checker(time)
